06b007255230072bedb083c51412f1140ab2765b
Mariusz Poslinski <mariusz.poslinski@nordicsemi.no>
2020-05-12 15:26:03 +0200

    FIX: Make zigbee_chedule API ISR/thread safe

--- subsys/zigbee/osif/zb_nrf_platform.h
+++ subsys/zigbee/osif/zb_nrf_platform.h
@@ -37,60 +37,52 @@ void zigbee_event_notify(zigbee_event_t event);
  */
 u32_t zigbee_event_poll(u32_t timeout_ms);
 
-/**@brief Schedule single-param callback execution and unlock
- *        Zigbee task afterwards.
+/**@brief Schedule single-param callback execution.
  *
- * Schedule execution of function `func' in the main scheduler loop.
+ * This API is thread- and ISR- safe.
+ * It performs all necessary actions:
+ *  - Forwards request from ISR to thread context
+ *  - Schedules the callback in ZBOSS scheduler queue
+ *  - Wakes up the Zigbee task.
  *
  * @param func    function to execute
- * @param param   callback parameter - usually, but not always
- *                ref to packet buffer
+ * @param param - callback parameter - usually ref to packet buffer
  *
  * @return RET_OK or RET_OVERFLOW.
  */
-static inline zb_ret_t zigbee_schedule_callback(zb_callback_t func,
-						zb_uint8_t param)
-{
-	zb_ret_t ret_code = ZB_SCHEDULE_APP_CALLBACK(func, param);
+zb_ret_t zigbee_schedule_callback(zb_callback_t func, zb_uint8_t param);
 
-	if (ret_code == RET_OK) {
-		zigbee_event_notify(ZIGBEE_EVENT_APP);
-	}
-	return ret_code;
-}
-
-/**@brief Schedule two-param callback execution and unlock
- *        Zigbee task afterwards.
+/**@brief Schedule two-param callback execution.
  *
- * Schedule execution of function `func' in the main scheduler loop.
+ * This API is thread- and ISR- safe.
+ * It performs all necessary actions:
+ *  - Forwards request from ISR to thread context
+ *  - Schedules the callback in ZBOSS scheduler queue
+ *  - Wakes up the Zigbee task.
  *
  * @param func        function to execute
- * @param param       zb_uint8_t callback parameter - usually, but not always
- *                    ref to packet buffer
- * @param user_param  zb_uint16_t user parameter - usually, but not always
- *                    short address
+ * @param param       zb_uint8_t callback parameter - usually, ref to packet buffer
+ * @param user_param  zb_uint16_t additional user parameter
  *
  * @return RET_OK or RET_OVERFLOW.
  */
-static inline zb_ret_t zigbee_schedule_callback2(zb_callback_t func,
-						 zb_uint8_t param,
-						 zb_uint16_t user_param)
-{
-	zb_ret_t ret_code = ZB_SCHEDULE_APP_CALLBACK2(func, param, user_param);
+zb_ret_t zigbee_schedule_callback2(zb_callback_t func, zb_uint8_t param,
+				   zb_uint16_t user_param);
 
-	if (ret_code == RET_OK) {
-		zigbee_event_notify(ZIGBEE_EVENT_APP);
-	}
-	return ret_code;
-}
 
-/**@brief Schedule alarm - callback to be executed after timeout
- *        and unlock Zigbee task afterwards.
+/**@brief Schedule alarm - callback to be executed after timeout.
  *
- * Function will be called via scheduler after timeout
- * expired (maybe, plus some additional time).
- * Timer resolution depends on implementation.
+ * Function will be called via scheduler after timeout expired, with possible
+ * delays due to locking/unlocking and scheduling mechanism.
+ * Timer resolution depends on implementation and is limited to a single
+ * Beacon Interval (15.36 msec).
  * Same callback can be scheduled for execution more then once.
+ *
+ * This API is thread- and ISR- safe.
+ * It performs all necessary actions:
+ *  - Forwards request from ISR to thread context
+ *  - Schedules the callback in ZBOSS scheduler queue
+ *  - Wakes up the Zigbee task.
  *
  * @param func       function to call via scheduler
  * @param param      parameter to pass to the function
@@ -98,16 +90,23 @@ static inline zb_ret_t zigbee_schedule_callback2(zb_callback_t func,
  *
  * @return RET_OK or RET_OVERFLOW
  */
-static inline zb_ret_t zigbee_schedule_alarm(zb_callback_t func,
-					     zb_uint8_t param,
-					     zb_time_t run_after)
-{
-	zb_ret_t ret_code = ZB_SCHEDULE_APP_ALARM(func, param, run_after);
-
-	if (ret_code == RET_OK) {
-		zigbee_event_notify(ZIGBEE_EVENT_APP);
-	}
-	return ret_code;
-}
+zb_ret_t zigbee_schedule_alarm(zb_callback_t func, zb_uint8_t param,
+			       zb_time_t run_after);
+/**@brief Cancel previously scheduler alarm.
+ *
+ * This API cancels alarms scheduled via zigbee_schedule_alarm() API
+ *
+ * This API is thread- and ISR- safe.
+ * It performs all necessary actions:
+ *  - Forwards request from ISR to thread context
+ *  - Schedules the callback in ZBOSS scheduler queue
+ *  - Wakes up the Zigbee task.
+ *
+ * @param func - function to call via scheduler
+ * @param param - parameter to pass to the function
+ *
+ * @return RET_OK or RET_OVERFLOW
+ */
+zb_ret_t zigbee_schedule_alarm_cancel(zb_callback_t func, zb_uint8_t param);
 
 #endif /* ZB_NRF_PLATFORM_H__ */
